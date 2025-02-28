717e8912d8f6c995315bc6d4abafd7613585bc6d
Luckaura <116108567+Luckaura@users.noreply.github.com>
2025-02-28 15:36:12 +0200

    Initial commit of Django project

---
+++ miskoris_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MiskorisAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'miskoris_app'
---
+++ miskoris_app/views.py
@@ -0,0 +1,10 @@
+﻿from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+  return render(request, "miskoris_app/home.html")
+
+def about(request):
+    return render(request, "miskoris_app/about.html")
