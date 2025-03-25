37ad4fd808c28a90a21a9fa02ceb1d52a22852ce
n1k1ta28 <niki.kisia@gmail.com>
2025-03-25 13:54:36 +0200

    password recovery implemented

--- miskoris_app/urls.py
+++ miskoris_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
+from . import views
 
 urlpatterns=[
   path('admin/', admin.site.urls),  
@@ -16,5 +17,9 @@ urlpatterns=[
   path('miskai/<int:id>/', views.forest, name='forest'),
   path('zemelapis/', views.mapPage, name="map"),
   path('tikrinimai/<int:id>/', views.orders, name="orders"),
-  path('tikrinimai/<int:forest_id>/<int:order_id>/tikrinimas/', views.order, name="order")
+  path('tikrinimai/<int:forest_id>/<int:order_id>/tikrinimas/', views.order, name="order"),
+  path('slaptazodzio_atkurimas/', auth_views.PasswordResetView.as_view(template_name="miskoris_app/password_reset.html"), name="reset_password"),
+  path('atkurimo_laiskas_issiustas/', auth_views.PasswordResetDoneView.as_view(template_name="miskoris_app/password_reset_sent.html"), name="password_reset_done"),
+  path('atkurimas/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="miskoris_app/password_reset_form.html"), name='password_reset_confirm'),
+  path('slapazodis_atkurtas/', auth_views.PasswordResetCompleteView.as_view(template_name="miskoris_app/password_reset_done.html"), name='password_reset_complete')
 ]
